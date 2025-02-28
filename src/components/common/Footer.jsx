b0d4b5cd40f8afe19d8fdd900ea0bde96984ea07
Divyanshu Kaushal <123375624+Divyanshu2125@users.noreply.github.com>
2025-02-28 19:58:38 +0530

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -188,7 +188,7 @@ const Footer = () => {
             <a href="https://www.linkedin.com/in/divyanshu-kaushal-80b0a9262/" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
               <ImLinkedin2 size={17} />
             </a>
-            <a href="https://www.github.com/vivek-panchal" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/Divyanshu2125" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
               <ImGithub size={17} />
             </a>
             <a href="https://x.com/DivyanshuK82514" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
